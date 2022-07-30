95708d4a15025bf2b1411169a83f1645770305eb
Osmait <saulburgos6@gmail.com>
2022-07-30 15:17:12 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Citas_react_Vite
---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { Formulario } from "./components/Formulario"
+import { Header } from "./components/Header"
+import { ListadoPacientes } from "./components/ListadoPacientes"
+import { useEffect, useState } from 'react'
+
+
+function App() {
+  const INITIAL = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+  const [pacientes, setPacientes] = useState(INITIAL);
+  const [paciente, setPaciente] = useState({});
+
+
+
+  useEffect(() => {
+    localStorage.setItem('pacientes', JSON.stringify( pacientes ));
+  }, [pacientes])
+
+
+  const eliminaPacientes = id => {
+    const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id);
+    setPacientes(pacientesActualizados)
+  }
+
+  return (
+    <div className="container mx-auto mt-20" >
+      <Header />
+
+      <div className="mt-12 md:flex">
+        <Formulario
+          pacientes={pacientes}
+          setPacientes={setPacientes}
+          paciente={paciente}
+          setPaciente={setPaciente}
+
+        />
+        <ListadoPacientes
+          pacientes={pacientes}
+          setPaciente={setPaciente}
+          eliminaPacientes={eliminaPacientes}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default App
