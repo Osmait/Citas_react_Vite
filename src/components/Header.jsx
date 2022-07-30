1f9dee52c0e76e117535dc1489018d8585a140fb
Osmait <saulburgos6@gmail.com>
2022-07-30 15:28:55 -0400

    No fue mucho trabajo pero fue un trabajo honesto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export const Header = () => {
     return (
         <>
             <h1 className='font-black text-5xl text-center  md:w-2/3 mx-auto'>
-                Seguimiento Pacientes {''}
+                Seguimiento Pacientes {''} CON GIThub{''}
                 <span className='text-indigo-600'>Veterinaria</span>
             </h1>
         </>
